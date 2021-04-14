87d80854171a86b9aac0ee5eed7c988c4c8ed501
IuliiaKalenyk <yulyaa.kalenyk@gmail.com>
2021-04-14 16:09:01 +0300

    готово

--- gallery-code.js
+++ gallery-code.js
@@ -26,31 +26,57 @@ navEl.insertAdjacentHTML('afterbegin', makeGalleryMarkupRows);
 console.log(gallery(galleryItems[0]));
 
 navEl.addEventListener('click', onOpenModal);
-function onOpenModal(evt) {
-    if (evt.target.nodeName !== IMG) {
-        return;
-    }
-    console.log(evt.target.textContent);
-}
 
 const refs = {
-    lightbox: document.querySelector('.lightbox'),
+    lightbox: document.querySelector('.js-lightbox'),
     closeModalBtn: document.querySelector('[data-action="close-lightbox"]'),
     overlay: document.querySelector('.lightbox__overlay'),
     content: document.querySelector('.lightbox__content'),
     image: document.querySelector('.lightbox__image'),
 }
-
+console.log(refs.image);
 
 refs.closeModalBtn.addEventListener('click', onCloseModal);
 refs.overlay.addEventListener('click', onCloseOverlay);
 
+function onOpenModal(evt) {
+    evt.preventDefault();
+    if (evt.target.nodeName === "IMG") {
+        refs.lightbox.classList.add("is-open");
+        refs.image.src = evt.target.dataset.source;
+        refs.image.alt = evt.target.alt;
+        refs.closeModalBtn.addEventListener("click", onCloseModal);
+        refs.lightbox.addEventListener("click", onlightboxImg);
+        refs.overlay.addEventListener("click", onCloseOverlay);
+    }
+};
+
+
 function onCloseModal() {
-    refs.closeModalBtn.classList.remove('is-open');
-}
+    refs.lightbox.classList.remove('is-open');
+    refs.image.src = '';
+    refs.image.alt = '';
+    /* refs.closeModalBtn.remove("click", onCloseModal);
+    refs.lightbox.remove("click", onlightboxImg);
+    refs.overlay.remove("click", onCloseOverlay); */
+};
 
 function onCloseOverlay(event) {
     if (event.currentTarget === event.target) {
         onCloseModal();
     }
-}
+};
+function onlightboxImg(event) {
+    if (event.target.nodeName !== "IMG") {
+        onCloseModal();
+    }
+};
+const lazyImages = document.querySelectorAll('img[loading = "lazy"]');
+
+lazyImages.forEach(image => {
+    image.addEventListener('load', onImageLoaded);
+});
+
+function onImageLoaded(evt) {
+    console.log('Картинка загрузилась');
+};
